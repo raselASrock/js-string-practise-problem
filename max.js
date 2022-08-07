dc1faced52e9596db43fb84db51cd80b64a0ee68
raselASrock <mdraselm413@gmail.com>
2022-08-08 03:06:56 +0600

    max-added

---
+++ max.js
@@ -0,0 +1,16 @@
+// const jim = 84;
+// const dela = 75;
+// if( jim > dela){
+//     console.log("Jim will get the cake.");
+// }
+// else{
+//     console.log("Dela will get the cake.");
+// }
+const jim = 69;
+const dela = 97;
+if( jim > dela){
+    console.log("Jim will get the cake.");
+}
+else{
+    console.log("Dela will get the cake.");
+}
