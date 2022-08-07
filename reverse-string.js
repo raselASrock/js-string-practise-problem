8680c5ba728c681f70a3f4e1a2b369366dd2f56b
raselASrock <mdraselm413@gmail.com>
2022-08-08 04:26:34 +0600

    string-reverse added

---
+++ reverse-string.js
@@ -0,0 +1,24 @@
+// function reverseString(text){
+//     for( i = 0; i < text.length; i++){
+//         const element = text[i];
+//         console.log(element);
+//     }
+// }
+
+// const myString = "I am a good boy"
+// const reversed = reverseString(myString);
+
+
+function reverseString(text){
+    let reversed = "";
+    for(let i = text.length-1; i >=0; i--){
+        const element = text[i];
+        reversed = reversed + element;
+        console.log(element, reversed);
+    }
+    return reversed;
+}
+
+const myString = "I am a good boy"
+const reversed = reverseString(myString);
+console.log("Reversed Output: ", reversed);
