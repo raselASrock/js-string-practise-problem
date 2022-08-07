0e3a6ffbee7a5f6ff9c1652f334b86e088cc82bc
raselASrock <mdraselm413@gmail.com>
2022-08-07 19:32:53 +0600

    first commit

---
+++ case.js
@@ -0,0 +1,10 @@
+const userName = "blackPink";
+const userInput = "blackPinK";
+console.log(userName.toLowerCase())
+console.log(userInput.toLowerCase())
+if(userName.toLowerCase() ===  userInput.toLowerCase()){
+    console.log("Valid User")
+}
+else{
+    console.log("Invalid User")
+}
